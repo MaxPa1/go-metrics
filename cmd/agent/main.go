1bedbbcd538d7078fcddf943da8ff420fcbac8cc
Palenov Maksim <palenov.mi@mge.mos.ru>
2026-07-22 20:25:54 +0300

    iter2 Агентский функционал

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,32 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"time"
+
+	"github.com/MaxPa1/go-metrics/internal/agent"
+)
+
+const (
+	pullInterval   = 2 * time.Second
+	reportInterval = 10 * time.Second
+)
+
+func main() {
+	client := &http.Client{
+		Timeout: time.Second * 5,
+	}
+	metricAgent := agent.NewMetricAgent()
+
+	lastReport := time.Now()
+
+	for {
+		metricAgent.UpdateMetrics()
+		time.Sleep(pullInterval)
+
+		if time.Since(lastReport) >= reportInterval {
+			metricAgent.SendMetrics(client)
+			lastReport = time.Now()
+		}
+	}
+}
